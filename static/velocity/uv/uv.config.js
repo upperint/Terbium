48bbf468b9a0eaca263195605bf42bef84df3d99
John Glynn <playingallday383@gmail.com>
2023-07-31 15:21:37 -0400

    Major Update

---
+++ static/velocity/uv/uv.config.js
@@ -0,0 +1,17 @@
+self.__uv$config = {
+  prefix: "/sw/",
+  bare: [
+    "https://uv.studentportal.lol/",
+    "https://uv.radon.games/",
+    "https://uv.holyubofficial.net/",
+    "https://tomp.app/",
+    "https://flow-os.liquid.is-a.dev/bare/"
+  ],
+  encodeUrl: Ultraviolet.codec.xor.encode,
+  decodeUrl: Ultraviolet.codec.xor.decode,
+  handler: "/uv/uv.handler.js",
+  client: "/uv/uv.client.js",
+  bundle: "/uv/uv.bundle.js",
+  config: "/uv/uv.config.js",
+  sw: "/uv/uv.sw.js",
+};
